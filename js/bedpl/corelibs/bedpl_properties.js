28ab016054bdccb927c64dac5997772be29af5a8
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2023-09-05 00:46:41 +0100

    Update bedpl_properties.js

--- js/bedpl/corelibs/bedpl_properties.js
+++ js/bedpl/corelibs/bedpl_properties.js
@@ -1,8 +1,8 @@
 // **************************************************************************************************
 // Description: BEDPL PROPERTIES
-// Version: 02-09-2023
-// Date create: 02-09-2023
-// Date update: 02-09-2023
+// Version: 1-2023
+// Date create: 05-09-2023
+// Date update: 05-09-2023
 // Author: Pedro Martins
 // License: MIT
 // File: pl_properties.js
