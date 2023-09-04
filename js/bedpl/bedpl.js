ea3088dde671b24ed3e5f1ea9e89f1fd37c5be1a
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2023-09-05 00:30:19 +0100

    Update bedpl.js

--- js/bedpl/bedpl.js
+++ js/bedpl/bedpl.js
@@ -8,6 +8,4 @@ document.write('<script src="./js/bedpl/corelibs/bedpl_properties_extra.js" type
 // libs
 // ******************************************
 document.write('<script src="./js/bedpl/libs/bedpl_body.js" type="text/javascript"></script>');
-document.write('<script src="./js/bedpl/libs/bedpl_bar.js" type="text/javascript"></script>');
-document.write('<script src="./js/bedpl/libs/bedpl_sub_bar.js" type="text/javascript"></script>');
 document.write('<script src="./js/bedpl/libs/bedpl_text.js" type="text/javascript"></script>');
